3eb6ba9d5ffd64ae66a3ed00714754ec319510d6
suzoagba <samuzoagba@gmail.com>
2022-09-14 15:31:39 +0300

    My Isnegative.go code

--- isnegative.go
+++ isnegative.go
@@ -1,20 +1,13 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-
-if nb > 0 {
-z01.PrintRune('T')
-z01.PrintRune('\n')
-} else { 
-z01.PrintRune('F')
-z01.PrintRune('\n')
-}
-}
-
-func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
+	if nb < 0 {
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
+	}
 }
