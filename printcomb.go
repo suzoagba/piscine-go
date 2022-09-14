60cb47351f7442c53e56ae1f007c337ee1406ce5
suzoagba <samuzoagba@gmail.com>
2022-09-14 20:47:25 +0300

    My Printcomb

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	Bob := false
+	for i := '0'; i <= '9'; i++ {
+		for j := i + 1; j <= '9'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				if Bob {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+
+				}
+				Bob = true
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
