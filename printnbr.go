6b0b558986f1bf032c2477c3b5fa5acb3e2d3125
suzoagba <samuzoagba@gmail.com>
2022-09-15 11:49:04 +0300

    My Printnbr

---
+++ printnbr.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func checkit(v int) {
+	c := '0'
+	if v == 0 {
+		z01.PrintRune(c)
+		return
+	}
+	for i := 1; i <= v%10; i++ {
+		c++
+	}
+	for i := -1; i >= v%10; i-- {
+		c++
+	}
+	if v/10 != 0 {
+		checkit(v / 10)
+	}
+	z01.PrintRune(c)
+	return
+}
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	checkit(n)
+}
