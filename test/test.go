5f9117a66044bbcb031044f894438f782425b558
suzoagba <samuzoagba@gmail.com>
2022-09-15 18:54:46 +0300

    Sortintegertable

--- test/test.go
+++ test/test.go
@@ -6,12 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Atoi("12345"))
-	fmt.Println(piscine.Atoi("0000000012345"))
-	fmt.Println(piscine.Atoi("012 345"))
-	fmt.Println(piscine.Atoi("Hello World!"))
-	fmt.Println(piscine.Atoi("+1234"))
-	fmt.Println(piscine.Atoi("-1234"))
-	fmt.Println(piscine.Atoi("++1234"))
-	fmt.Println(piscine.Atoi("--1234"))
+	s := []int{5, 4, 3, 2, 1, 0}
+	piscine.SortIntegerTable(s)
+	fmt.Println(s)
 }
