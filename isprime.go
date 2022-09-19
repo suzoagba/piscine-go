eeec6ed362aa0717d859b011f23c8ae2f80e9c95
suzoagba <samuzoagba@gmail.com>
2022-09-19 21:21:18 +0300

    isprime.go

--- isprime.go
+++ isprime.go
@@ -1,15 +1,19 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	c := 1
-	for i := 2; i <= nb; i += 2 {
-		if nb%i == 0 {
-			c++
-		}
-	}
-	if c == 2 {
-		return true
-	} else {
+	if nb <= 1 {
 		return false
 	}
+	if nb == 2 {
+		return true
+	}
+	if nb%2 == 0 {
+		return false
+	}
+	for i := 3; i <= nb/3; i += 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
 }
