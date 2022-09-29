4b32e965c4ede879b9a56f05ef888cbac440683a
suzoagba <samuzoagba@gmail.com>
2022-09-29 10:19:10 +0300

    test

---
+++ listsize.go
@@ -0,0 +1,11 @@
+package piscine
+
+func ListSize(l *List) int {
+	size := 0
+
+	for l.Head != nil {
+		size++
+		l.Head = l.Head.Next
+	}
+	return size
+}
--- test/test.go
+++ test/test.go
@@ -8,13 +8,9 @@ import (
 func main() {
 	link := &piscine.List{}
 	piscine.ListPushFront(link, "Hello")
+	piscine.ListPushFront(link, "2")
+	piscine.ListPushFront(link, "you")
 	piscine.ListPushFront(link, "man")
-	piscine.ListPushFront(link, "how are you")
 
-	it := link.Head
-	for it != nil {
-		fmt.Print(it.Data, " ")
-		it = it.Next
-	}
-	fmt.Println()
+	fmt.Println(piscine.ListSize(link))
 }
