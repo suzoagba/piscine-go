4f16aae75f55c0a4238a965e4efc5d33d0572893
suzoagba <samuzoagba@gmail.com>
2022-09-29 10:43:26 +0300

    listforeach

--- test/test.go
+++ test/test.go
@@ -7,20 +7,15 @@ import (
 
 func main() {
 	link := &piscine.List{}
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, 2)
-	piscine.ListPushBack(link, 3)
-	piscine.ListPushBack(link, 4)
-
-	piscine.ListReverse(link)
+	piscine.ListPushBack(link, "1")
+	piscine.ListPushBack(link, "2")
+	piscine.ListPushBack(link, "3")
+	piscine.ListPushBack(link, "5")
+	piscine.ListForEach(link, piscine.Add2_node)
 
 	it := link.Head
-
 	for it != nil {
 		fmt.Println(it.Data)
 		it = it.Next
 	}
-
-	fmt.Println("Tail", link.Tail)
-	fmt.Println("Head", link.Head)
 }
