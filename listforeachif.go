f5fd0fe8145b39159310f7c506a4db70ea3ddc52
suzoagba <samuzoagba@gmail.com>
2022-09-29 10:47:41 +0300

    listforeachif

---
+++ listforeachif.go
@@ -0,0 +1,30 @@
+package piscine
+
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	default:
+		return false
+	}
+}
+
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	default:
+		return true
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	a := l.Head
+
+	for a != nil {
+		if cond(a) == true {
+			f(a)
+		}
+		a = a.Next
+	}
+}
